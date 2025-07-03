6b68367fde031cd941038d6d6c4e34a3755b734c
forexdark <gabrielpsiravegna@gmail.com>
2025-07-03 14:01:58 -0300

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -28,7 +28,7 @@ app.include_router(router)
 if __name__ == "__main__":
     start_scheduler()
     port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
 
 @app.get("/")
 def root():
